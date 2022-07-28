a9e427acdae9d93ce9a09e1ea65e8870e36e5953
suslmk <67575226+suslmk-lee@users.noreply.github.com>
2022-07-29 05:08:31 +0900

    Initial Commit

---
+++ deeplink_swiftui_tutorial/deeplink_swiftui_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  deeplink_swiftui_tutorialApp.swift
+//  deeplink_swiftui_tutorial
+//
+//  Created by minkyuLee on 2022/07/29.
+//
+
+import SwiftUI
+
+@main
+struct deeplink_swiftui_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
